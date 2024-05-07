c16ccb2a8f73ac91e31d39f1052cc5cb0cdbf7ad
Jhow-Magnum <jhowmagnumweb@outlook.com>
2024-05-06 22:38:02 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.services.ConsumoAPI;
+import br.com.alura.screenmatch.services.ConverterDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Gilmore+Girls&Season=1&apikey=84b0f20c");
+		//System.out.println(json);
+		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		ConverterDados conversor = new ConverterDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+
+
+
+	}
+}
